e9a8897570a49778b47decb0d1a2bf86720a46a0
VadymAOA <vadym@alphaomega.agency>
2023-10-16 15:16:13 +0300

    splitting settings files

--- real_estate/settings/development.py
+++ real_estate/settings/development.py
@@ -4,7 +4,7 @@ DATABASES = {
     'default':{
         'ENGINE': env("POSTGRESS_ENGINE"),
         'NAME': env('POSTGRES_DB'),
-        'USER':env('POSTGRES_USERNAME'),
+        'USER':env('POSTGRES_USER'),
         'PASSWORD':env('POSTGRES_PASSWORD'),
         'HOST':env('PG_HOST'),
         'PORT':env('PG_PORT')
