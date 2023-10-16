3b495fc2df208bd65bb62b6c6cf2bd68ffe2d89e
VadymAOA <vadym@alphaomega.agency>
2023-10-16 17:50:37 +0300

    add custom user model

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import User
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    ordering = ['email'] # задает порядок сортировки в административном интерфейсе по электронной почте.
+    add_form = CustomUserCreationForm # add_form и form - это формы, используемые для создания и изменения пользователя соответственно.
+    form = CustomUserChangeForm
+    model = User
+    list_display = ['pkid', 'id', 'email', 'username', 'first_name', 'last_name', 'is_staff', 'is_active']
+    list_display_links = ['id', 'email']
+    list_filter = ['email', 'username', 'first_name', 'last_name', 'is_staff', 'is_active']
+    # определяет, какие поля будут отображаться и группироваться в административном интерфейсе редактирования пользователя.
+    fieldsets = (
+        (
+            _("Login Credentials"),
+            {
+                "fields": ("email", "password", )
+            },
+        ),
+        (
+            _("Personal Information"),
+            {"fields":("username", "first_name", "last_name",)}
+        ),
+        (
+            _("Permissions and Groups"),
+            {
+                "fields":(
+                "is_active", 
+                "is_staff", 
+                "is_superuser",
+                "groups", 
+                "user_permissions",
+                )
+            },
+        ),
+        (
+            _("Important Dates"), {"fields": ("last_login", "date_joined")}
+        ),
+    )
+    # определяет, какие поля будут отображаться при создании нового пользователя.
+    add_fieldsets=(
+            (None,{
+                "classes": ("wide",), # "classes": ("wide",) в определении add_fieldsets является настройкой стилей для административной формы Django. Это указывает, что форма должна быть широкой, что может быть полезным, например, для обеспечения лучшей видимости полей при создании пользователя.
+                "fields": ("email", "password1", "password2", "is_staff", "is_active"),
+            }
+        )
+    )
+    search_fields = ["email", "username", "first_name", "last_name"]
+
+admin.site.register(User, UserAdmin)
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,47 @@
+import uuid
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from .managers import CustomUserManage
 
-# Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    pkid = models.BigAutoField(primary_key=True, editable=False) # оле для первичного ключа с типом BigAutoField. Это большое целочисленное поле с автоматическим инкрементом.
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True) # Поле для уникального идентификатора пользователя типа UUIDField. Уникальный идентификатор генерируется автоматически с использованием uuid.uuid4().
+    username = models.CharField(verbose_name=_('Username'), max_length=255, unique=True)
+    first_name = models.CharField(verbose_name=_('First Name'), max_length=50)
+    last_name = models.CharField(verbose_name=_('Last Name'), max_length=50)
+    email = models.EmailField(verbose_name=_("Email Address"), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now) # Поле, отражающее дату и времени присоединения пользователя.
+
+    '''
+    USERNAME_FIELD и REQUIRED_FIELDS: Определение поля, которое будет использоваться в качестве уникального идентификатора пользователя (в данном случае, email). REQUIRED_FIELDS - это список полей, которые требуются для создания пользователя.
+    '''
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=[ 'username', 'first_name', 'last_name']
+
+    objects = CustomUserManage() # Менеджер пользователей, определенный в CustomUserManager (предполагается, что он определен в файле managers.py).
+
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
+
+    def __str__(self):
+        return self.username # Возвращает строковое представление пользователя, в данном случае - его имя пользователя.
+    
+
+    '''
+    get_full_name и get_short_name методы: Предоставляют полное и короткое имя пользователя соответственно.
+    '''
+    @property
+    def get_full_name(self):
+        return f'{self.first_name.title()} {self.last_name.title()}'
+    
+    def get_short_name(self):
+        return self.username
+
+
+
+
