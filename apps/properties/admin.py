de2d402be21867602e6affd0483ef82192ad175d
VadymAOA <vadym@alphaomega.agency>
2023-10-25 12:01:13 +0300

    Add properties ane enquiries

---
+++ apps/properties/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Property, PropertyViews
+
+class PropertyAdmin(admin.ModelAdmin):
+    list_display =["title", "country", "advert_type", "property_type"]
+
+admin.site.register(Property, PropertyAdmin)
+admin.site.register(PropertyViews)
